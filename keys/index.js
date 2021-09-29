096c3f104dddebbc1858137434d29f0aa6c45388
Semyon Sigunava <49509717+sigunavaboi@users.noreply.github.com>
2021-09-29 13:14:21 +0700

    Remove secrets

--- keys/index.js
+++ keys/index.js
@@ -1,9 +1,3 @@
-module.exports = {
-  MONGO_URI: 'mongodb+srv://sigunawa:dF47jlgqQdVEjwWg@cluster0.98w5t.mongodb.net/shop',
-  DEFAULT_PORT: 3000,
-  SECRET_SESSION: 'some secret session',
-};
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys.prod');
 } else {
